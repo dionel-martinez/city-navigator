884370f1d88d69deb69e9ad0f1fd80d68dd613ad
Milton Pagan <milton.pagan1@upr.edu>
2021-04-27 14:44:55 -0400

    Changed to more realistic road speeds

--- city-navigator/src/maps/PuertoRicoClosed.java
+++ city-navigator/src/maps/PuertoRicoClosed.java
@@ -12,41 +12,41 @@ public class PuertoRicoClosed extends PuertoRico {
         createRoads();
     }
 
-    protected void createRoads() {
+    protected void addRoads() {
         LinkedList<Road> roads = new LinkedList<Road>();
 
         roads.add(new Road(ponce, santaIsabel, 24, 100, TrafficCongestionDegree.CLOSED));
         roads.add(new Road(salinas, santaIsabel, 12, 100, TrafficCongestionDegree.MEDIUM));
         roads.add(new Road(salinas, cayey, 28, 100));
-        roads.add(new Road(salinas, arroyo, 33, 100));
-        roads.add(new Road(arroyo, humacao, 50, 100));
-        roads.add(new Road(humacao, fajardo, 40, 100));
+        roads.add(new Road(salinas, arroyo, 33, 90));
+        roads.add(new Road(arroyo, humacao, 50, 90));
+        roads.add(new Road(humacao, fajardo, 40, 90));
         roads.add(new Road(humacao, caguas, 31, 100));
-        roads.add(new Road(canovanas, fajardo, 32, 100));
-        roads.add(new Road(canovanas, caguas, 36, 100));
+        roads.add(new Road(canovanas, fajardo, 32, 90));
+        roads.add(new Road(canovanas, caguas, 36, 90));
         roads.add(new Road(sanJuan, canovanas, 30, 100));
         roads.add(new Road(sanJuan, vegaBaja, 47, 100, TrafficCongestionDegree.CLOSED));
         roads.add(new Road(sanJuan, caguas, 33, 100));
         roads.add(new Road(cayey, caguas, 25, 100, TrafficCongestionDegree.CLOSED));
-        roads.add(new Road(cayey, barranquitas, 30, 100));
-        roads.add(new Road(barranquitas, santaIsabel, 40, 100));
-        roads.add(new Road(barranquitas, villalba, 33, 100));
-        roads.add(new Road(ponce, villalba, 30, 100, TrafficCongestionDegree.HIGH));
-        roads.add(new Road(ponce, adjuntas, 25, 100, TrafficCongestionDegree.CLOSED));
-        roads.add(new Road(ponce, yauco, 35, 100));
-        roads.add(new Road(yauco, sanGerman, 27, 100));
-        roads.add(new Road(yauco, lajas, 30, 100));
-        roads.add(new Road(lajas, sanGerman, 5, 100));
-        roads.add(new Road(sanGerman, mayaguez, 20, 100));
-        roads.add(new Road(mayaguez, aguadilla, 28, 100));
-        roads.add(new Road(lares, adjuntas, 32, 100));
-        roads.add(new Road(lares, arecibo, 29, 100));
-        roads.add(new Road(lares, mayaguez, 47, 100));
-        roads.add(new Road(aguadilla, arecibo, 50, 100));
-        roads.add(new Road(arecibo, manati, 30, 100));
-        roads.add(new Road(manati, barranquitas, 55, 100));
+        roads.add(new Road(cayey, barranquitas, 30, 70));
+        roads.add(new Road(barranquitas, santaIsabel, 40, 70));
+        roads.add(new Road(barranquitas, villalba, 33, 70));
+        roads.add(new Road(ponce, villalba, 30, 70, TrafficCongestionDegree.HIGH));
+        roads.add(new Road(ponce, adjuntas, 25, 70, TrafficCongestionDegree.CLOSED));
+        roads.add(new Road(ponce, yauco, 35, 90));
+        roads.add(new Road(yauco, sanGerman, 27, 90));
+        roads.add(new Road(yauco, lajas, 30, 90));
+        roads.add(new Road(lajas, sanGerman, 5, 90));
+        roads.add(new Road(sanGerman, mayaguez, 20, 90));
+        roads.add(new Road(mayaguez, aguadilla, 28, 90));
+        roads.add(new Road(lares, adjuntas, 32, 70));
+        roads.add(new Road(lares, arecibo, 29, 70));
+        roads.add(new Road(lares, mayaguez, 47, 70));
+        roads.add(new Road(aguadilla, arecibo, 50, 90));
+        roads.add(new Road(arecibo, manati, 30, 90));
+        roads.add(new Road(manati, barranquitas, 55, 70));
         roads.add(new Road(manati, vegaBaja, 10, 100));
-        roads.add(new Road(vegaBaja, barranquitas, 47, 100));
+        roads.add(new Road(vegaBaja, barranquitas, 47, 70));
 
         for (Road road : roads) {
             addRoadBetweenCities(road.getCity1(), road.getCity2(), road.getDistance(), road.getSpeedLimit(),
